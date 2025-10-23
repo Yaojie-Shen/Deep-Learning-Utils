6d761823a2263eb79a65bb274233bd5682058f35
Yaojie Shen <acherstyx@gmail.com>
2025-10-23 15:05:41 +0800

    Update recursive_to

--- dl_utils/distributed/device.py
+++ dl_utils/distributed/device.py
@@ -8,36 +8,39 @@ __all__ = [
     "recursive_to",
 ]
 
-from typing import Any, Union
+from typing import Any
 
 import torch
 
 
-def recursive_to(obj: Any, device: Union[str, torch.device] = None) -> Any:
+def recursive_to(obj: Any, *args, **kwargs) -> Any:
     """
-    Recursively move all torch.Tensor in obj to the given device.
+    Recursively move all torch.Tensor in obj to the given device/dtype following the same behavior as torch.Tensor.to().
     Supports: Tensor, list, tuple, dict, set. Leaves other objects intact.
 
     Args:
         obj: The object to move.
-        device: The device to move to. If None, uses the current device if gpu is available, else "cpu".
+        **kwargs: Keyword arguments to pass to torch.Tensor.to().
 
     Returns:
         The object with all torch.Tensor moved to the given device.
+
+    Note:
+        If no device is specified, the current (gpu) device will be used. If no gpu is available, the cpu will be used.
     """
-    if device is None:
-        device = torch.cuda.current_device() if torch.cuda.is_available() else "cpu"
+    if (not args) and (not kwargs):
+        kwargs['device'] = torch.cuda.current_device() if torch.cuda.is_available() else "cpu"
 
     if isinstance(obj, torch.Tensor):
-        return obj.to(device)
+        return obj.to(*args, **kwargs)
     elif isinstance(obj, dict):
-        return {k: recursive_to(v, device) for k, v in obj.items()}
+        return {k: recursive_to(v, *args, **kwargs) for k, v in obj.items()}
     elif isinstance(obj, list):
-        return [recursive_to(v, device) for v in obj]
+        return [recursive_to(v, *args, **kwargs) for v in obj]
     elif isinstance(obj, tuple):
-        return tuple(recursive_to(v, device) for v in obj)
+        return tuple(recursive_to(v, *args, **kwargs) for v in obj)
     elif isinstance(obj, set):
         # sets are unordered; converting back to set might lose type/sort
-        return {recursive_to(v, device) for v in obj}
+        return {recursive_to(v, *args, **kwargs) for v in obj}
     else:
         return obj
